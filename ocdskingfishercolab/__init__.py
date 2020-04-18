2cb4c0e19482826a5e674915551791fade1b6099
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-04-18 12:57:54 -0400

    Add domains to docstrings. Capitalize SQL keywords. Fix quoting style.

--- ocdskingfishercolab/__init__.py
+++ ocdskingfishercolab/__init__.py
@@ -27,6 +27,9 @@ conn = None
 def create_connection(database, user, password, host, port='5432'):
     """
     Creates a connection to the database.
+
+    :returns: a database connection
+    :rtype: psycopg2.extensions.connection
     """
     global conn
     if conn and conn.closed:
@@ -56,6 +59,9 @@ def reset_connection():
 def authenticate_gspread():
     """
     Authenticates the current user and gives the notebook permission to connect to Google Spreadsheets.
+
+    :returns: a `Google Sheets Client <https://gspread.readthedocs.io/en/latest/api.html#client>`__ instance
+    :rtype: gspread.Client
     """
     auth.authenticate_user()
     return gspread.authorize(GoogleCredentials.get_application_default())
@@ -64,7 +70,10 @@ def authenticate_gspread():
 def authenticate_pydrive():
     """
     Authenticates the current user and gives the notebook permission to connect to Google Drive.
-    """
+
+    :returns: a `GoogleDrive <https://gsuitedevs.github.io/PyDrive/docs/build/html/pydrive.html#module-pydrive.drive>`__ instance
+    :rtype: pydrive.drive.GoogleDrive
+    """  # noqa: E501
     auth.authenticate_user()
     gauth = GoogleAuth()
     gauth.credentials = GoogleCredentials.get_application_default()
@@ -74,6 +83,10 @@ def authenticate_pydrive():
 def getResults(cur):
     """
     Takes in a database cursor and returns a Pandas DataFrame.
+
+    :param psycopg2.extensions.cursor cur: a database cursor
+    :returns: The results as a data frame
+    :rtype: pandas.DataFrame
     """
     headers = [desc[0] for desc in cur.description]
     return pandas.DataFrame(cur.fetchall(), columns=headers)
@@ -82,6 +95,9 @@ def getResults(cur):
 def saveToCSV(dataframe, filename):
     """
     Takes in a Pandas DataFrame and save the data to a file in the notebook.
+
+    :param pandas.DataFrame dataframe: a data frame
+    :param str filename: a file name
     """
     dataframe.to_csv(filename)
     files.download(filename)
@@ -90,6 +106,8 @@ def saveToCSV(dataframe, filename):
 def set_spreadsheet_name(name):
     """
     Sets the name of the spreadsheet to save. Used by saveStraightToSheets.
+
+    :param str name: a sheet name
     """
     global spreadsheet_name
     spreadsheet_name = name
@@ -97,50 +115,57 @@ def set_spreadsheet_name(name):
 
 def saveStraightToSheets(dataframe, sheetname):
     """
-    Saves a DataFrame straight to a Google Spreadsheet.
+    Saves a data frame to Google Sheets, without asking the user for conformation.
+
+    :param pandas.DataFrame dataframe: a data frame
+    :param str sheetname: a sheet name
     """
     gc = authenticate_gspread()
-    # open or create gSheet
     try:
-        gSheet = gc.open(spreadsheet_name)
+        sheet = gc.open(spreadsheet_name)
     except Exception:
-        gSheet = gc.create(spreadsheet_name)
+        sheet = gc.create(spreadsheet_name)
 
     try:
-        worksheet = gSheet.add_worksheet(sheetname, dataframe.shape[0], dataframe.shape[1])
+        worksheet = sheet.add_worksheet(sheetname, dataframe.shape[0], dataframe.shape[1])
     except Exception:
-        newsheetname = input(sheetname + ' already exists, enter a different name: ')
-        worksheet = gSheet.add_worksheet(newsheetname, dataframe.shape[0], dataframe.shape[1])
+        newsheetname = input('{} already exists, enter a new name:'.format(sheetname))
+        worksheet = sheet.add_worksheet(newsheetname, dataframe.shape[0], dataframe.shape[1])
 
     set_with_dataframe(worksheet, dataframe)
 
 
 def saveToSheets(dataframe, sheetname):
     """
-    The same as saveStraightToSheets, except the user is prompted if they are sure first.
+    Saves a data frame to Google Sheets, after asking the user for confirmation.
+
+    :param pandas.DataFrame dataframe: a data frame
+    :param str sheetname: a sheet name
     """
-    if input('Save to Google Sheets? (y/n)') == 'y':
+    if input('Save to Google Sheets? (y/N)') == 'y':
         saveStraightToSheets(dataframe, sheetname)
 
 
 def downloadReleases(collection_id, ocid, package_type):
     """
     Downloads some releases into a file in the notebook.
+
+    :param int collection_id: a collection's ID
+    :param str ocid: an OCID
+    :param str package_type: "record" or "release"
     """
     if package_type not in ('record', 'release'):
         print("package_type parameter must be either 'release' or 'record'")
         return
 
     querystring = """
-    with releases as (
-        select
+    WITH releases AS (
+        SELECT
             ocid,
             data
-        from
-            data
-        join
-            release_with_collection on data.id = release_with_collection.data_id
-        where collection_id = %(collection_id)s
+        FROM data
+        JOIN release_with_collection ON data.id = release_with_collection.data_id
+        WHERE collection_id = %(collection_id)s
     )
     SELECT
       jsonb_build_object('releases', jsonb_agg(data)),
@@ -148,16 +173,14 @@ def downloadReleases(collection_id, ocid, package_type):
           'ocid', %(ocid)s,
           'records', jsonb_build_array(jsonb_build_object('releases', jsonb_agg(data)))
       )
-    FROM
-      releases
-    WHERE
-      ocid = %(ocid)s
+    FROM releases
+    WHERE ocid = %(ocid)s
     """
 
     with conn, conn.cursor() as cur:
         cur.execute(querystring, {'ocid': ocid, 'collection_id': collection_id})
 
-        file = ocid + '_' + package_type + '_package.json'
+        file = '{}_{}_package.json'.format(ocid, package_type)
 
         result = cur.fetchone()
         if package_type == 'release':
@@ -174,6 +197,11 @@ def downloadReleases(collection_id, ocid, package_type):
 def output_notebook(sql, params=None):
     """
     Runs a SQL query and returns results to the notebook.
+
+    :param str sql: a SQL statement
+    :param params: the parameters to pass to the SQL statement
+    :returns: The results as a data frame
+    :rtype: pandas.DataFrame
     """
     with conn, conn.cursor() as cur:
         try:
