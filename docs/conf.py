b9155ce55b01ef4b4d1bba530b4cd95fcf98d7a4
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-04-21 14:40:16 -0400

    Iterate the version number

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = '2019, Open Contracting Partnership'
 author = 'Open Contracting Partnership'
 
 # The short X.Y version
-version = '0.2.0'
+version = '0.2.1'
 # The full version, including alpha/beta/rc tags
 release = version
 
