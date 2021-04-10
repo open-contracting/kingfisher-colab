8e0e0bbccbad256ae8427798a40d33ed9e5424ee
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-04-10 18:14:22 -0400

    release: Iterate the version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='ocdskingfishercolab',
-    version='0.3.2',
+    version='0.3.3',
     author='Open Contracting Partnership',
     author_email='data@open-contracting.org',
     url='https://github.com/open-contracting/kingfisher-colab',
