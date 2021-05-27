fc8d85bb2c518486ea1f5825635d01b1952255c4
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-05-27 16:55:13 -0400

    setup: Fix package name (sphinx-rtd-theme)

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'docs': [
             'Sphinx',
             'sphinx-autobuild',
-            'sphinx_rtd_theme',
+            'sphinx-rtd-theme',
         ],
     },
     classifiers=[
